dadde11ce3dc26be6a88fb54eb39b17349357b3e
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-07-02 12:27:22 +0200

    CI: fixed path bug for script

--- .ci_scripts/firewall-test/add_nft_rules.sh
+++ .ci_scripts/firewall-test/add_nft_rules.sh
@@ -1,6 +1,6 @@
 EXITCODE=0
 
-for nft_script in $GITHUB_WORKSPACE/devices/*/firewall*.nft
+for nft_script in $GITHUB_WORKSPACE/test/translator/devices/*/firewall.nft
 do
     # Flush the ruleset before next device
     sudo nft flush ruleset
