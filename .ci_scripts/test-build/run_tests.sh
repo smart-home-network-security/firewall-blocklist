742b6f802ee6f4a870bc8778d4c9c87241619d30
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2025-08-06 16:01:59 +0200

    Setup Github CI

--- .ci_scripts/test-build/run_tests.sh
+++ .ci_scripts/test-build/run_tests.sh
@@ -1,6 +1,6 @@
 EXITCODE=0
 PARSERS_DIR="src/parsers"
-VALGRIND_SUPP=".ci_scripts/native-build/valgrind.supp"
+VALGRIND_SUPP=".ci_scripts/test-build/valgrind.supp"
 
 PREFIX=""
 for file in bin/test/* "$PARSERS_DIR"/bin/test/*
